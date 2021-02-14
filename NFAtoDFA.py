fbfb6682e09c4e01db7e5ef6aaf355ab60b8654f
not-legato <tommirossidude@gmail.com>
2021-02-14 21:59:19 +0200

    NFA -> DFA conversion added.
    The code is still a little clumsy and must be redesigned later, but for now, it works.

--- NFAtoDFA.py
+++ NFAtoDFA.py
@@ -2,10 +2,10 @@ from itertools import chain, combinations # for powersets
 
 # This model of a DFA is completely equivalent to the formal definition.
 class DFA:
-    def __init__(self, Q: set, Sigma: set, delta, q0: str, F: set):
+    def __init__(self, Q: set, Sigma: set, delta, q0, F: set):
         self.states = Q
         self.alphabet = Sigma
-        self.transition = delta
+        self.delta = delta
         self.start = q0
         self.accept = F
         self.position = q0
@@ -15,7 +15,7 @@ class DFA:
         if not set(s).issubset(self.alphabet):
             return False
         for c in s:
-            self.position = self.transition(self.position, c)
+            self.position = self.delta(self.position, c)
         if self.position in self.accept:
             self.resetPosition()
             return True
@@ -25,6 +25,18 @@ class DFA:
 
     def resetPosition(self):
         self.position = self.start
+
+    def __str__(self):
+        representations = []
+        rep = ""
+        for state in self.states:
+            rep += f"{state}: " + "{"
+            for c in self.alphabet:
+                rep += f"{c} -> {self.delta(state, c)}, "
+            rep = rep[:-2] + "}"
+            representations.append(rep)
+            rep = ""
+        return "\n".join(sorted(representations))
     
 # for now, this NFA model only serves to be converted to DFA.
 # delta must be a function tuple -> tuple: delta((states), input) = (more states)
@@ -32,80 +44,92 @@ class NFA:
     def __init__(self, Q: dict, Sigma: set, delta, q0: str, F: set):
         self.states = Q
         self.alphabet = Sigma
-        self.delta = delta
+        self.delta = delta # this shows what arrows are connected to what state.
         self.start = q0
         self.accept = F
 
-def powerset(data: set):
-    return chain.from_iterable(combinations(data, r) for r in range(len(data) + 1))
+    def __str__(self):
+        representations = []
+        rep = ""
+        for state in self.states:
+            rep += f"{state}: " + "{"
+            for c in self.alphabet:
+                temp = "{" + ", ".join(self.delta(state, c)) + "}"
+                rep += f"{c} -> {temp}, "
+            rep = rep[:-2] + "}"
+            representations.append(rep)
+            rep = ""
+        return "\n".join(sorted(representations))
 
-# this is via the powerset construction.
+def powerset(data: set):
+    Pdata = tuple([*chain.from_iterable(combinations(data, r) for r in range(len(data) + 1))])
+    return Pdata
+
+# powerset construction.
 def NFAtoDFA(M: NFA):
-    Q = powerset(M.states)
-    F = (R for R in Q if set(R).intersection(M.accept)) # if R contains an accept state, add.
+    Q_prime = powerset(M.states)
+    F_prime = set([R for R in Q_prime if set(R).intersection(M.accept)]) # all subsets of P(Q) containing an accept state.
+
+    # R is an element of Q' = P(Q).
+    # deltaprime = d'(R,a) = {q in Q : q in d(r,a) for r in R}, where
+    # d refers to M.arrows() and Q to M.states.
+    def deltaprime(R: tuple, input: str):
+        union = ()
+        for state in R:
+            try:
+                union += M.delta(state, input)
+            except:
+                continue
+        return union
 
     # E(R) as defined in Michael Sipser's "Introduction to the Theory of Computation".
-    # takes a set of states and adds all the epsilon-connected states.
+    # takes a set (tuple) of states and appends all states connected by outgoing
+    # epsilon-arrows.
     def E(states: tuple):
-        epsilon_paths = tuple()
+        epsilon_paths = ()
         for state in states:
             if deltaprime(state, "epsilon"): # not empty
                 epsilon_paths += (deltaprime(state, "epsilon"))
-        return tuple(states + epsilon_paths)
+        return states + epsilon_paths
 
-    # define deltaprime d' as: for R in Q', d'(R,a) = {q in Q : q in d(r,a) for r in R}.
-    # Here Q = M.states and Q' = powerset(M.states).
-    def deltaprime(states: tuple, input: str):
-        union = tuple()
-        for state in states:
-            union += (M.delta(state, input))
-        return tuple(union)
-
-    # redefine delta to include epsilon-paths.
-    def delta(states: tuple, input: str):
-        return E(deltaprime(states, input))
+    # redefine d' to include epsilon-paths.
+    def delta_prime(states: tuple, input: str):
+        return tuple(sorted(list(set(E(deltaprime(states, input)))))) # oh no.
 
     q0 = E(tuple(M.start)) # start state must contain empty string connections.
 
     # the theoretical construction is now complete.
-    return DFA(Q, M.alphabet, delta, q0, F)
+    return DFA(Q_prime, M.alphabet, delta_prime, q0, F_prime)
 
 
 # if a state has no arrows pointing to it (and is not a start state),
-# remove from machine.
+# remove from machine. TODO.
 def stripDeadEnds(M: DFA):
     pass
 
-# produces a proper transition function out of the dictionary of connections.
 def transition_function(instructions: dict):
-    def delta(state: str, input: str):
+    def delta(state, input):
         return instructions[state][input]
     return delta
 
 if __name__ == "__main__":
 
-    Q = {"a", "b", "c", "d"}            # possible states
-    Sigma = {"0", "1"}                  # alphabet
-    delta = transition_function({       # transition function
-        "a": {"0": "b", "1": "a"}, \
-        "b": {"0": "c", "1": "a"}, \
-        "c": {"0": "c", "1": "d"}, \
-        "d": {"0": "d", "1": "d"}})
-    q0 = "a"                            # starting state
-    F = {"d"}                           # set of accept states
-    M = DFA(Q, Sigma, delta, q0, F)     # finished machine
-    print(M.read("1110100111010111011"))
-
-    # this is for testing purposes, once the NFAtoDFA function works as intended.
-    # # this is Example 1.21 from Sipser.
-    # Q = {"1", "2", "3"}
-    # Sigma = {"a", "b"}
-    # delta = transition_function({"1": {"b": ("2",), "epsilon": ("3",)}, \
-    #                             "2": {"a": ("2", "3"), "b": ("3",)}, \
-    #                             "3": {"a": ("1", "3")}})
-    # q0 = "1"
-    # F = {"1"}
-    # N = NFA(Q, Sigma, delta, q0, F)
-    # M = NFAtoDFA(N)
-    # M.read("babba")
-    # M.read("baba")
+    # # this is Example 1.16 from Sipser.
+    Q = ("1", "2", "3")
+    Sigma = ("a", "b")
+    delta = transition_function({
+        "1": {"b": ("2",), "epsilon": ("3",)}, \
+        "2": {"a": ("2", "3"), "b": ("3",)}, \
+        "3": {"a": ("1", "3")}})
+    q0 = "1"
+    F = ("1",)
+    N = NFA(Q, Sigma, delta, q0, F)
+    M = NFAtoDFA(N)
+    trues = ["", "a", "baba", "baa"]
+    falses = ["b", "bb", "babba"]
+    print("These should be true.")
+    for i in trues:
+        print(M.read(i))
+    print("These should be false.")
+    for j in falses:
+        print(M.read(j))
